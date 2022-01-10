a040df3122e72312e782bdb0238a23a7e2a77fb2
exthitikorn <rockerzeeza@gmail.com>
2022-01-10 23:24:44 +0700

    vue-line-liff

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2 align="center">Workshop 1 - Limit Message</h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  beforeCreate () {
+    this.$liff.init(function (data) {})
+  },
+  openWindow (){
+    this.$liff.openWindow({
+      url: 'https://line.me'
+      })
+  },
+  closeWindow (){
+    liff.closeWindow();
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
