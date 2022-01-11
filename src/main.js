79345c0de977781044ef4b821f673bc41075d00a
exthitikorn <rockerzeeza@gmail.com>
2022-01-11 09:52:23 +0700

    add port

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
 Vue.prototype.$liff = window.$liff
 Vue.config.productionTip = false
 
